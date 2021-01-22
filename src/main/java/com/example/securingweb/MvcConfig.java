646e0a6449be9f25136410be71406be83bf3535a
Ivan Shilyaev <shilyaev1433@gmail.com>
2021-01-22 16:36:01 +0300

    Change login and home pages
    Change configuration. There is no controller yet. All redirects happens automatically

--- src/main/java/com/example/securingweb/MvcConfig.java
+++ src/main/java/com/example/securingweb/MvcConfig.java
@@ -1,16 +1,19 @@
 package com.example.securingweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/anonymous.html");
+        registry.addViewController("/login.html");
+        registry.addViewController("/homepage.html");
+        registry.addViewController("/admin/adminpage.html");
+        registry.addViewController("/accessDenied");
     }
 }
